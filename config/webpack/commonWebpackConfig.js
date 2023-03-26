6c4900a451903b827311270c1d0938a551f01df2
khanzardar <k.zardar@gmail.com>
2023-03-26 13:42:54 -0400

    updating webpackconfig.js for .module.scss

--- config/webpack/commonWebpackConfig.js
+++ config/webpack/commonWebpackConfig.js
@@ -25,22 +25,24 @@ const commonOptions = {
           "postcss-loader",
         ],
       },
-      // {
-      //   test: /\.modules\.scss$/,
-      //   use: [
-      //     "style-loader",
-      //     {
-      //       loader: "css-loader",
-      //       options: {
-      //         modules: {
-      //           localIdentName: "[name]__[local]___[hash:base64:5]",
-      //         },
-      //       },
-      //     },
-      //     "postcss-loader",
-      //     "sass-loader",
-      //   ],
-      // },
+      {
+        test: /\.modules\.scss$/,
+        use: [
+          process.env.NODE_ENV === "production"
+            ? MiniCssExtractPlugin.loader
+            : "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                localIdentName: "[name]__[local]___[hash:base64:5]",
+              },
+            },
+          },
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
       {
         test: /^((?!\.modules).)*\.scss$/,
         use: [
