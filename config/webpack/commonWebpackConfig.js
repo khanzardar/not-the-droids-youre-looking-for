48afba07322c3f5692e92d128b446590ea169127
khanzardar <k.zardar@gmail.com>
2023-03-27 16:18:02 -0400

    Changing module.scss based off of Webpack docs

--- config/webpack/commonWebpackConfig.js
+++ config/webpack/commonWebpackConfig.js
@@ -4,7 +4,6 @@ const {
 } = require("shakapacker");
 
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-// const isProduction = process.env.NODE_ENV === "production";
 
 const commonOptions = {
   resolve: {
@@ -12,12 +11,10 @@ const commonOptions = {
   },
   module: {
     rules: [
+      // SCSS ALL EXCEPT MODULES
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.module\.scss$/,
+        test: /\.scss$/i,
+        exclude: /\.module\.scss$/i,
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
@@ -25,7 +22,29 @@ const commonOptions = {
           {
             loader: "css-loader",
             options: {
+              importLoaders: 1,
               modules: {
+                mode: "icss",
+              },
+            },
+          },
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      // SCSS MODULES
+      {
+        test: /\.module\.scss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: {
+                mode: "local",
                 localIdentName: "[path][name]__[local]--[hash:base64:5]",
               },
             },
@@ -43,99 +62,3 @@ const commonWebpackConfig = () =>
   merge({}, baseClientWebpackConfig, commonOptions);
 
 module.exports = commonWebpackConfig;
-
-// // The source code including full typescript support is available at:
-// // https://github.com/shakacode/react_on_rails_demo_ssr_hmr/blob/master/config/webpack/commonWebpackConfig.js
-
-// // Common configuration applying to client and server configuration
-// const {
-//   webpackConfig: baseClientWebpackConfig,
-//   merge,
-// } = require("shakapacker");
-
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
-// // const commonOptions = {
-// //   resolve: {
-// //     extensions: [".css", ".ts", ".tsx", ".modules.scss"],
-// //   },
-// // };
-
-// // const sassLoaderConfig = {
-// //   loader: "sass-resources-loader",
-// //   options: {
-// //     resources: "./client/app/assets/styles/app-variables.scss",
-// //   },
-// // };
-
-// // const ignoreWarningsConfig = {
-// //   ignoreWarnings: [
-// //     /Module not found: Error: Can't resolve 'react-dom\/client'/,
-// //   ],
-// // };
-
-// // const scssConfigIndex = baseClientWebpackConfig.module.rules.findIndex(
-// //   (config) => ".scss".match(config.test)
-// // );
-
-// // baseClientWebpackConfig.module.rules[scssConfigIndex].use.push(
-// //   sassLoaderConfig
-// // );
-
-// // const commonWebpackConfig = () =>
-// //   merge({}, baseClientWebpackConfig, commonOptions, ignoreWarningsConfig);
-
-// const commonOptions = {
-//   resolve: {
-//     extensions: [".css", ".ts", ".tsx", ".modules.scss"],
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.css$/,
-//         use: [
-//           process.env.NODE_ENV === "production"
-//             ? MiniCssExtractPlugin.loader
-//             : "style-loader",
-//           "css-loader",
-//           "postcss-loader",
-//         ],
-//       },
-//       {
-//         test: /\.modules\.scss$/,
-//         use: [
-//           process.env.NODE_ENV === "production"
-//             ? MiniCssExtractPlugin.loader
-//             : "style-loader",
-//           {
-//             loader: "css-loader",
-//             options: {
-//               modules: {
-//                 localIdentName: "[name]__[local]___[hash:base64:5]",
-//               },
-//             },
-//           },
-//           "postcss-loader",
-//           "sass-loader",
-//         ],
-//       },
-//       {
-//         test: /^((?!\.modules).)*\.scss$/,
-//         use: [
-//           process.env.NODE_ENV === "production"
-//             ? MiniCssExtractPlugin.loader
-//             : "style-loader",
-//           "css-loader",
-//           "postcss-loader",
-//           "sass-loader",
-//         ],
-//       },
-//     ],
-//   },
-// };
-
-// //Copy the object using merge b/c the baseClientWebpackConfig and commonOptions are mutable globals
-// const commonWebpackConfig = () =>
-//   merge({}, baseClientWebpackConfig, commonOptions);
-
-// module.exports = commonWebpackConfig;
