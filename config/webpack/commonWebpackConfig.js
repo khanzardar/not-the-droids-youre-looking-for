ac695ddb28848f95ef85aaa8ba99eaa35f8cda17
khanzardar <k.zardar@gmail.com>
2023-03-28 08:34:57 -0400

    Changing MiniCssExtractPlugin related

--- config/webpack/commonWebpackConfig.js
+++ config/webpack/commonWebpackConfig.js
@@ -13,27 +13,46 @@ const commonOptions = {
     rules: [
       {
         test: /\.module\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: {
-              modules: {
-                localIdentName: "[name]__[local]___[hash:base64:5]",
-              },
-              sourceMap: true,
-            },
-          },
-          "sass-loader",
-        ],
-      },
-      {
-        test: /^((?!\.module).)*\.scss$/,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[fullhash].css",
+    }),
+  ],
 };
+
+// const commonOptions = {
+//   resolve: {
+//     extensions: [".css", ".js", ".jsx"],
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.module\.scss$/,
+//         use: [
+//           MiniCssExtractPlugin.loader,
+//           {
+//             loader: "css-loader",
+//             options: {
+//               modules: {
+//                 localIdentName: "[name]__[local]___[hash:base64:5]",
+//               },
+//               sourceMap: true,
+//             },
+//           },
+//           "sass-loader",
+//         ],
+//       },
+//       {
+//         test: /^((?!\.module).)*\.scss$/,
+//         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+//       },
+//     ],
+//   },
+// };
 
 // Copy the object using merge b/c the baseClientWebpackConfig and commonOptions are mutable globals
 const commonWebpackConfig = () =>
